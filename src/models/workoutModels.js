2b7f471ad72a76bb536a35ca44ee4d2704b31fa1
Etxebe <mikelgartzia21@gmail.com>
2023-10-18 13:24:42 +0200

    [FIX] corregido workoutModels

--- src/models/workoutModels.js
+++ src/models/workoutModels.js
@@ -1,1 +1,15 @@
-zyy
+//Cargamos el módulo de mongoose
+const mongoose = require('mongoose');
+
+//Usaremos los esquemas
+const {Schema} = mongoose;
+
+//Creamos el objeto del esquema y sus atributos
+const workoutSchema = new Schema({
+    name: String,
+    mode: String,
+    equipment: [String]
+});
+
+//Exportamos el modelo para usarlo en otros ficheros 
+module.exports = mongoose.model('Workout', workoutSchema);
